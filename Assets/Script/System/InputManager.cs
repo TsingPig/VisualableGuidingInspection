07bf1ea1f32e5ec211cddd82177ff338ab297340
TsingPig <1114196607@qq.com>
2023-09-12 23:25:42 +0800

    添加内容：Selectable接口

--- Assets/Script/System/InputManager.cs
+++ Assets/Script/System/InputManager.cs
@@ -1,24 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Highlighters;
 using TsingPigSDK;
+using System.Collections.Generic;
+
+public enum SelectMode
+{
+    Single,
+    Multi,
+    Null
+}
+
 public class InputManager : Singleton<InputManager>
 {
+    private List<ISelectable> _selectables = new List<ISelectable>(); 
+
+    private bool _canSelect = false;
+
+    private SelectMode _selectMode= SelectMode.Single;
+    public bool CanSelect { get => _canSelect; set => _canSelect = value; }
+    public SelectMode SelectMode { get => _selectMode; set => _selectMode = value; }
 
     private void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.Mouse0))
-        //{
-        //    RaycastHit hit;
-        //    Ray ray=Camera.main.ScreenPointToRay(Input.mousePosition);
-        //    if(Physics.Raycast(ray, out hit))
-        //    {
-        //        if (hit.collider.CompareTag("Patient"))
-        //        {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.CompareTag("Patient"))
+                {
+                    Transform patient = hit.collider.transform;
 
-        //        }
-        //    }
-        //}
+                }
+            }
+        }
     }
     private void Init()
     {
