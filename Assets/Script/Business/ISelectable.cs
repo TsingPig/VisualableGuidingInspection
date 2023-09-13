06bc4ec94ac43f54fec799486bfd4468d3510fe1
TsingPig <1114196607@qq.com>
2023-09-13 18:38:10 +0800

    添加内容：调整美术

--- Assets/Script/Business/ISelectable.cs
+++ Assets/Script/Business/ISelectable.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Highlighters;
 public interface ISelectable {
+	Highlighter Highlighter { get; }
 	void OnSelected();
 	void OffSelected();
 }
