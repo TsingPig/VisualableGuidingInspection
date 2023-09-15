3f3f3e0e46c831e64a3196a1710af2a1bcbc14ec
TsingPig <1114196607@qq.com>
2023-09-15 21:11:42 +0800

    添加功能：相机的平滑切换

--- Assets/Script/System/InputManager.cs
+++ Assets/Script/System/InputManager.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using TsingPigSDK;
 using Cinemachine;
+using System;
+
 public enum SelectMode
 {
     Single,
@@ -10,19 +12,22 @@ public enum SelectMode
 
 public class InputManager : Singleton<InputManager>
 {
-    private CinemachineVirtualCameraBase _cam;
+    private int _curCamIdx = 0;
+
+    private CinemachineBrain _brain;
+
+    private CinemachineVirtualCameraBase[] _cams;
 
     private Transform _cinemachineVirtualCameraTarget;
-
     public Transform CinemachineVirtualCameraTarget
     {
         get => _cinemachineVirtualCameraTarget;
         set
         {
-            _cam.Follow = value;
-            _cam.LookAt = value;
+            _cams[1].Follow = value;
+            _cams[1].LookAt = value;
         }
-      
+
     }
 
     MyList<ISelectable> _selectables = new MyList<ISelectable>();
@@ -35,6 +40,12 @@ public class InputManager : Singleton<InputManager>
 
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            _cams[_curCamIdx].enabled = false;
+            _curCamIdx = (_curCamIdx + 1) % _cams.Length;
+             _cams[_curCamIdx].enabled=true;
+        }
 
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
@@ -101,7 +112,8 @@ public class InputManager : Singleton<InputManager>
 
     private void Init()
     {
-        _cam=FindObjectOfType<CinemachineVirtualCameraBase>();
+        _brain = FindObjectOfType<CinemachineBrain>();
+        _cams = FindObjectsOfType<CinemachineVirtualCameraBase>();
         _selectables.OnItemAdded_Event += (ISelectable selectable) => selectable.OnSelected();
         _selectables.OnItemRemoved_Event += (ISelectable selectable) => selectable.OffSelected();
     }
